929e1b2d13732f1c0a64fda3eb6825a119c36beb
Armando Amador <Arm.amador@gmail.com>
2014-02-23 22:47:56 -0500

    Add a StaticPages Controller and install devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepolish::Application.config.secret_token = '08ef6103e668c317510c36ff563f86126274b9ad346f6121399965555263b470ae0d1e8f473e4d4ab84c998c75a2c370419fb6020f07fcf3161abe56d8d04156'
